7888593fed6cdf8771d143c7ae4d04b07187330c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-11 20:51:37 -0300

    EX4

---
+++ src/UsoMetodoConstrutor/EX4/MonitorEnergia.java
@@ -0,0 +1,31 @@
+package UsoMetodoConstrutor.EX4;
+
+public class MonitorEnergia {
+    double[] consumo;
+    int contador;
+
+    public MonitorEnergia(int tamanho) {
+        this.consumo = new double[tamanho];
+    }
+    public void registrar(double valor){
+        if (contador<consumo.length) {
+            consumo[contador] = valor;
+            contador++;
+        }
+    }
+    public double calcularMedia(){
+        double media=0;
+        for (double valor: consumo){
+            media+=valor;
+        }
+        return media/consumo.length;
+    }
+    public double calcularMedia(int inicio, int fim){
+        double media=0;
+        for (int i=inicio; i<fim;i++){
+            media+=consumo[i];
+
+        }
+        return media/(fim-inicio);
+    }
+}
